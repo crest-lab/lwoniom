8443511655db8996581cff91f85942f6c5fea7e7
Philipp Pracht <pp555@cam.ac.uk>
2023-06-20 15:13:38 +0100

    Some work on initialization

--- src/lwoniom_setup.f90
+++ src/lwoniom_setup.f90
@@ -47,6 +47,7 @@ module lwoniom_setup
 
   contains
     procedure :: deallocate => lwoniom_data_deallocate
+    procedure :: add_fragment => lwoniom_add_fragment
   end type lwoniom_data
 
 !========================================================================================!
@@ -66,24 +67,43 @@ contains  !> MODULE PROCEDURES START HERE
   end subroutine print_lwoniom_results
 !========================================================================================!
 
-  subroutine lwoniom_initialize(nat,at,xyz,dat,fname, &
+  subroutine lwoniom_initialize(nat,at,xyz,dat, &
+  &                 layer,center,               &
   &                 print,verbose,iunit,iostat)
+!******************************************************************
+!* This routine sets up the lwoniom_data object and handles
+!* the partitioning of the whole system into different layers.
+!*
+!* It requires input coordinates for the entire system, as
+!* well as "layering" information.
+!* The layering instruction can be provided by one or two arrays:
+!*  A) If only the "layer" array is provided, layers will contain
+!*     all the atoms corresponting to the respective layer
+!*  B) If the "layer" and the "center" arrays are provided,
+!*     one layer can have mutliple fragments (MC-ONIOM)
+!*
+!******************************************************************
     character(len=*),parameter :: source = 'lwoniom_initialize'
     !> INPUT
-    integer,intent(in) :: nat
-    integer,intent(in) :: at(nat)
-    real(wp),intent(in) :: xyz(3,nat)
-    character(len=*),intent(in) :: fname
+    integer,intent(in) :: nat          !> number of atoms    
+    integer,intent(in) :: at(nat)      !> atom number for each atom
+    real(wp),intent(in) :: xyz(3,nat)  !> Cartesian coordinates for each atom
+    integer,intent(in) :: layer(nat)   !> layer instruction for each atom
+    integer,intent(in),optional :: center(nat) !> center instruction (for MC-ONIOM)
     logical,intent(in),optional  :: print
     logical,intent(in),optional  :: verbose
     integer,intent(in),optional  :: iunit
-    integer,intent(out),optional :: iostat
     !> OUTPUT
+    integer,intent(out),optional :: iostat
     type(lwoniom_data),intent(inout) :: dat
     !> LOCAL
+    type(structure_data) :: tmp
+    integer,allocatable :: center_tmp(:)
     integer :: ich,io,myunit
     logical :: ex,okbas,pr,pr2
     logical :: exitRun
+
+    io = 0
 
 !> mapping of optional instuctions
     if (present(print)) then
@@ -103,10 +123,25 @@ contains  !> MODULE PROCEDURES START HERE
       myunit = stdout
     end if
 
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! ROUTINE CONTENT GOES FROM HERE                           
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    allocate(center_tmp(nat), source=0)
+    if(present(center))then
+        center_tmp(:) = center(:)
+    endif
+
+
+
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!  TO HERE                           
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 !> lwONIOM calculator setup goes here
     if ((io /= 0).and.pr) then
-      write (myunit,'("Could not create force field calculator ",a)') source
+      write (myunit,'("Could not create lwONIOM object ",a)') source
     end if
     if (present(iostat)) then
       iostat = io
@@ -114,7 +149,11 @@ contains  !> MODULE PROCEDURES START HERE
   end subroutine lwoniom_initialize
 
 !========================================================================================!
+
   subroutine lwoniom_data_deallocate(self)
+!****************************************************
+!* This subroutine deallocates a lwoniom_data object
+!*****************************************************
     implicit none
     class(lwoniom_data) :: self
     self%lwoniom_energy = 0.0_Wp
@@ -122,6 +161,38 @@ contains  !> MODULE PROCEDURES START HERE
     self%nfrag = 0
     if (allocated(self%fragment)) deallocate (self%fragment)
   end subroutine lwoniom_data_deallocate
+
+
+!========================================================================================!
+  subroutine lwoniom_add_fragment(self,frag)
+!*****************************************************
+!* This subroutine adds a single structure_data object
+!* to the fragment list of a lwoniom_data object
+!******************************************************
+    implicit none
+    class(lwoniom_data) :: self
+    type(structure_data) :: frag
+    type(structure_data),allocatable :: tmp(:)
+    integer :: n,n1
+    if(frag%layer > self%nlayer)then
+       self%nlayer = frag%layer
+    endif
+    if(self%nfrag == 0)then
+      self%nfrag = 1
+      allocate(self%fragment(1))
+      self%fragment(1) = frag
+    else
+      n = self%nfrag
+      n1 = n + 1
+      allocate(tmp(n1))
+      tmp(1:n) = self%fragment(1:n)
+      tmp(n1) = frag
+      call move_alloc(tmp,self%fragment) 
+      self%nfrag = n1
+    endif
+  end subroutine lwoniom_add_fragment
+
+
 
 !========================================================================================!
 end module lwoniom_setup
